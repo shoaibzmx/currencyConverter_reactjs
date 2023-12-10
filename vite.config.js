952b8637c19e7cdc1bfcc30307a6e38c786b9f24
Shoaib Hussain <shoaibhussain4095@gmail.com>
2023-12-10 17:15:38 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/currencyConverter_reactjs/",
+  base:"/currencyConverter_reactjs",
   plugins: [react()],
 })
